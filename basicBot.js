f07ef224f91d9a407e7d39c4364c6908dbf8ec72
Viscerals <Viscerals@users.noreply.github.com>
2015-05-01 20:16:51 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -183,7 +183,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+        scriptLink: "https://raw.githubusercontent.com/Viscerals/basicBot/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
